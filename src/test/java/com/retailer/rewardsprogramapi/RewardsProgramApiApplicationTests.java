e136854e2a704e80e9dfbc0db0b15251c7fa8f80
Rahul Kaup <rahulkaup@gmail.com>
2023-09-22 10:38:17 -0700

    added java spring boot skeleton using initializr

---
+++ src/test/java/com/retailer/rewardsprogramapi/RewardsProgramApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retailer.rewardsprogramapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardsProgramApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
